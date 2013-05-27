6e257c3f156b125319ec367eae68d763b913dd72
Jim <jimjohns@mac.com>
2013-05-27 16:13:03 +0100

    27mai2013-emailOK

--- Gite/Contact_files/Contact.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 2.0.4 local-build-20130527
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Contact_files/ContactMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
--- maison/contact_files/contact.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 2.0.4 local-build-20130527
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('contact_files/contactMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
