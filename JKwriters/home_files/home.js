596c5d6fe271bc9fdd1aeb1c1782110078458ccf
Jim <jimjohns@mac.com>
2013-06-03 18:17:05 +0100

    3juin2013

--- JKwriters/home_files/home.js
+++ JKwriters/home_files/home.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20130527
+// Created by iWeb 2.0.4 local-build-20130603
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,326),url:'home_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'home_files/stroke_1.png'},{rect:new IWRect(5,-5,214,10),url:'home_files/stroke_2.png'},{rect:new IWRect(219,-5,10,10),url:'home_files/stroke_3.png'},{rect:new IWRect(219,5,10,326),url:'home_files/stroke_4.png'},{rect:new IWRect(219,331,10,10),url:'home_files/stroke_5.png'},{rect:new IWRect(5,331,214,10),url:'home_files/stroke_6.png'},{rect:new IWRect(-5,331,10,10),url:'home_files/stroke_7.png'}],new IWSize(224,336))});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('home_files/homeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
